201beae1ac8088661450c07e23c33e642e3c3840
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-11-24 18:20:27 +0530

    Implement Navigation Drawer

--- app/src/main/java/com/example/expense_manager/HomeActivity.java
+++ app/src/main/java/com/example/expense_manager/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.expense_manager;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -18,11 +23,61 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
         toolbar.setTitle("Expense Manager");
-       // setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navView);
+       navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.END)){
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    public void displaySelectedListener(int itemId){
+        Fragment fragment = null;
+
+        switch (itemId){
+            case R.id.dashboard:
+                break;
+
+            case R.id.income:
+                break;
+
+            case R.id.expense:
+                break;
+        }
+
+        if(fragment!=null){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_frame,fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        displaySelectedListener(item.getItemId());
+        return true;
     }
 }
