95d59cca5e4551aa6c286732689af5c7d2aca236
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-11-24 13:51:08 +0530

    Registration Functionality Integrating Firebase

--- app/src/main/java/com/example/expense_manager/RegistrationActivity.java
+++ app/src/main/java/com/example/expense_manager/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.example.expense_manager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -16,10 +24,18 @@ public class RegistrationActivity extends AppCompatActivity {
     Button btnReg;
     TextView mSignin;
 
+    private ProgressDialog mDialog;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDialog = new ProgressDialog(this);
 
         registration();
     }
@@ -47,6 +63,26 @@ public class RegistrationActivity extends AppCompatActivity {
                     mPass.setError("Password Required..");
                     return;
                 }
+
+                mDialog.setMessage("Processing..");
+                mDialog.show();
+
+                mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        }
+                        else {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Registration Failed", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
 
