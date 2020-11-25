51221e1660fec267ff62d6446c33ff7cb807bab5
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-11-25 13:11:07 +0530

    Connect Custom Dialog

--- app/src/main/java/com/example/expense_manager/DashboardFragment.java
+++ app/src/main/java/com/example/expense_manager/DashboardFragment.java
@@ -1,18 +1,26 @@
 package com.example.expense_manager;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -29,6 +37,10 @@ private boolean isOpen = false;
 
 private Animation FadOpen,FadClose;
 
+FirebaseAuth mAuth;
+private DatabaseReference mIncomeDatabase,mExpenseDatabase;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,6 +48,13 @@ private Animation FadOpen,FadClose;
         addData();
         // Inflate the layout for this fragment
         View myview =  inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+
+        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseData").child(uid);
 
         fab_main_btn = myview.findViewById(R.id.fb_main_plus_btn);
         fab_income_btn = myview.findViewById(R.id.income_ft_btn);
@@ -85,16 +104,66 @@ private Animation FadOpen,FadClose;
         fab_income_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                incomeDataInsert();
             }
         });
 
         fab_expense_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
+    }
+
+    public void incomeDataInsert(){
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myviewm = inflater.inflate(R.layout.custom_layout_for_insertdata,null);
+        mydialog.setView(myviewm);
+        final AlertDialog dialog = mydialog.create();
+
+        final EditText edtAmount = myviewm.findViewById(R.id.amount_edt);
+        final EditText edtType = myviewm.findViewById(R.id.type_edt);
+        final EditText edtNote = myviewm.findViewById(R.id.note_edt);
+
+        Button btnSave = myviewm.findViewById(R.id.btnSave);
+        Button btnCancel = myviewm.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type = edtType.getText().toString().trim();
+                String amount = edtAmount.getText().toString().trim();
+                String note = edtNote.getText().toString().trim();
+
+                if(TextUtils.isEmpty(type)){
+                    edtType.setError("Required Filed..");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(amount)){
+                    edtAmount.setError("Required Filed..");
+                    return;
+                }
+
+                int ouramountint = Integer.parseInt(amount);
+
+                if(TextUtils.isEmpty(note)){
+                    edtNote.setError("Required Filed..");
+                    return;
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 
 }
